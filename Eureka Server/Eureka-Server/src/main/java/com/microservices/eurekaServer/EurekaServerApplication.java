38b6d8474974e0dc126149f35bf2a3af40265d7e
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2023-12-03 11:57:24 +0530

    till apiGateway

--- Eureka Server/Eureka-Server/src/main/java/com/microservices/eurekaServer/EurekaServerApplication.java
+++ Eureka Server/Eureka-Server/src/main/java/com/microservices/eurekaServer/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.microservices.User;
+package com.microservices.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class UserApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
